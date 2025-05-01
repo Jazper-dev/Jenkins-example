5ea5ca6f3b0811dab025711edcf27398e94f15f2
jammy11 <67936544+jammy11@users.noreply.github.com>
2025-05-01 22:10:45 +0700

    ok

--- auto-deploy/src/App.tsx
+++ auto-deploy/src/App.tsx
@@ -11,16 +11,17 @@ function App() {
 
       </div>
       <h1>Auto Deploy</h1>
-      <a href="">#test 2</a>
+    
       <iframe
         width="560"
         height="315"
-        src="https://youtu.be/VUh4cVfvgK0?si=6b7shGibDwmhsbc1"
+        src="https://www.youtube.com/embed/VUh4cVfvgK0?si=6b7shGibDwmhsbc1"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
+
       <div className="card">
 
         <button onClick={() => setCount((count) => count + 1)}>
